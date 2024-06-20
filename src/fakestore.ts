039bbe14c5e2a8b6a8ba0da2768fb42755174bf1
pablobeckg <beckgarreaud@gmail.com>
2024-06-20 16:09:24 +0200

    last push

--- src/fakestore.ts
+++ src/fakestore.ts
@@ -7,11 +7,21 @@ const mensClothing = document.getElementById('mens-clothing') as HTMLButtonEleme
 const womensClothing = document.getElementById('womens-clothing') as HTMLButtonElement;
 const searchField = document.getElementById('search-field') as HTMLInputElement;
 const sortBy = document.getElementById('sort-by') as HTMLSelectElement;
+const loadingIndicator = document.getElementById('loading') as HTMLDivElement;
+const darkMode = document.querySelector('.dark-mode') as HTMLButtonElement;
+const getBody = document.querySelector('body') as HTMLBodyElement;
+const category = document.getElementById('mobile-menu') as HTMLSelectElement;
 
 const BASE_URL = 'https://fakestoreapi.com/';
 const PRODUCTS_URL = `${BASE_URL}products`;
 let allProducts: IProducts[] = [];
 let filteredProducts: IProducts[] = [];
+let currentFilteredProducts: IProducts[] = [];
+
+let isElectronicsSelected = false;
+let isJewelerysSelected = false;
+let isMensSelected = false;
+let isWomensSelected = false;
 
 function fetchClothing() {
     fetch(PRODUCTS_URL)
@@ -30,25 +40,29 @@ function fetchClothing() {
   fetchClothing();
 
   function displayProducts(products: IProducts[]): void  {
-    const createEachProduct = products.map((product: IProducts) => {  
-      return `
+    loadingIndicator.style.display = 'block';
+    productDisplay.innerHTML = ''
+    setTimeout(() => {
+      loadingIndicator.style.display = 'none';
+      const createEachProduct = products.map((product: IProducts) => {  
+        return `
         <div class="product-container">
-            <img class="picture" src="${product.image}" alt="">
-            <h3>${product.title}</h3>
-            <div class="price-and-button-div">
-              <h2>$ ${product.price.toFixed(2)}</h2>
-              <button class="add-to-cart">Add to cart</button>
-            </div>
+        <img class="picture" src="${product.image}" alt="">
+        <h3>${product.title}</h3>
+        <div class="price-and-button-div">
+        <h2>$ ${product.price.toFixed(2)}</h2>
+        <button class="add-to-cart">Add to cart</button>
+        </div>
         </div>
         `;
-    });
-    productDisplay.innerHTML = createEachProduct.join('');
+      });
+      productDisplay.innerHTML = createEachProduct.join('');
+    }, 50000);
 }
-
-
 
 function displayPressedCategory(products: IProducts[], tag: string) {
   filteredProducts = products.filter((product: IProducts) => product.category === tag);
+  currentFilteredProducts = [...filteredProducts];
   displayProducts(filteredProducts);
 }
 
@@ -59,38 +73,52 @@ function searchProducts(products: IProducts[], tag: string) {
   displayProducts(filteredProducts);
 }
 
-
 electronics.addEventListener('click',(event: Event) => {
   event.preventDefault();
   displayPressedCategory(allProducts, 'electronics');
+  isElectronicsSelected = true;
+  isJewelerysSelected = false;
+  isMensSelected = false;
+  isWomensSelected = false;
+  changeButtonColor()
 });
 
 jewelery.addEventListener('click',(event: Event) => {
   event.preventDefault();
   displayPressedCategory(allProducts, 'jewelery');
+  isElectronicsSelected = false;
+  isJewelerysSelected = true;
+  isMensSelected = false;
+  isWomensSelected = false;
+  changeButtonColor()
 });
 
 mensClothing.addEventListener('click',(event: Event) => {
   event.preventDefault();
   displayPressedCategory(allProducts, "men's clothing");
+  isElectronicsSelected = false;
+  isJewelerysSelected = false;
+  isMensSelected = true;
+  isWomensSelected = false;
+  changeButtonColor()
 });
 
 womensClothing.addEventListener('click',(event: Event) => {
   event.preventDefault();
   displayPressedCategory(allProducts, "women's clothing");
+  isElectronicsSelected = false;
+  isJewelerysSelected = false;
+  isMensSelected = false;
+  isWomensSelected = true;
+  changeButtonColor()
 });
 
-searchField.addEventListener('input',(event: Event) => {
-  event.preventDefault();
-  if(filteredProducts.length >= 1) {
-    searchProducts(filteredProducts, searchField.value);
-    filteredProducts = []
-  
-  } else {
-    searchProducts(allProducts, searchField.value);
-
-  }
-});
+function changeButtonColor() {
+  electronics.style.backgroundColor = isElectronicsSelected ? 'rgb(203, 142, 27)' : '';
+  jewelery.style.backgroundColor = isJewelerysSelected ? 'rgb(203, 142, 27)' : '';
+  mensClothing.style.backgroundColor = isMensSelected ? 'rgb(203, 142, 27)' : '';
+  womensClothing.style.backgroundColor = isWomensSelected ? 'rgb(203, 142, 27)' : '';
+}
 
 sortBy.addEventListener('change', (event: Event) => {
   event.preventDefault();
@@ -101,4 +129,46 @@ sortBy.addEventListener('change', (event: Event) => {
   }
   displayProducts(filteredProducts);
   sortBy.value = '';
+});
+
+
+searchField.addEventListener('input',(event: Event) => {
+  event.preventDefault();
+  const searchValue = searchField.value.trim();
+
+  if (searchValue === '') {
+
+    displayProducts(currentFilteredProducts.length > 0 ? currentFilteredProducts : allProducts);
+  } else {
+
+    searchProducts(currentFilteredProducts.length > 0 ? currentFilteredProducts : allProducts, searchValue);
+  }
+});
+
+darkMode.addEventListener('click', () => {
+  if (darkMode.textContent === 'Dark Mode') {
+    darkMode.textContent = 'Light Mode';
+    getBody.style.backgroundColor = 'black';
+    darkMode.style.color = 'black';
+    darkMode.style.backgroundColor = 'white';
+  } else {
+    darkMode.textContent = 'Dark Mode';
+    getBody.style.backgroundColor = '#efefee';
+    darkMode.style.color = 'white';
+    darkMode.style.backgroundColor = 'black';
+  }
+});
+
+category.addEventListener('change', (event: Event) => {
+  event.preventDefault();
+  if (category.value === 'electronics') {
+      displayPressedCategory(allProducts, 'electronics');
+    } else if (category.value === 'jewelery') {
+      displayPressedCategory(allProducts, 'jewelery')
+    } else if (category.value === 'mens-clothing') {
+      displayPressedCategory(allProducts, "men's clothing")
+    } else if (category.value === 'womens-clothing') {
+      displayPressedCategory(allProducts, "women's clothing")
+    }
+  category.value = '';
 });
