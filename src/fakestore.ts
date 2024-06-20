f662e298905a2d03f70fc69bd04d5529b74b9691
JulianSupercode <jscheib1305@gmail.com>
2024-06-20 10:52:54 +0200

    neue Funktionen

--- src/fakestore.ts
+++ src/fakestore.ts
@@ -7,24 +7,49 @@ const mensClothing = document.getElementById('mens-clothing') as HTMLButtonEleme
 const womensClothing = document.getElementById('womens-clothing') as HTMLButtonElement;
 const searchField = document.getElementById('search-field') as HTMLInputElement;
 const sortBy = document.getElementById('sort-by') as HTMLSelectElement;
+const clearFiltersButton = document.getElementById('clear-filters') as HTMLButtonElement;
+const storeTitle = document.getElementById('store-title') as HTMLHeadingElement;
+
 
 const BASE_URL = 'https://fakestoreapi.com/';
 const PRODUCTS_URL = `${BASE_URL}products`;
 let allProducts: IProducts[] = [];
+let filteredProducts: IProducts [] =[];
+let isFiltered: boolean = false;
 
+
+// function fetchClothing() {
+//     fetch(PRODUCTS_URL)
+//     .then((response: Response) => {
+//         if (!response.ok) {
+//             throw Error(`${response.status} ${response.statusText}`);
+//           }
+//           return response.json();
+//     })
+//     .then((products: IProducts[]) => {
+//         displayProducts(products);
+//         allProducts = products;
+//     })
+//   }
 function fetchClothing() {
-    fetch(PRODUCTS_URL)
-    .then((response: Response) => {
-        if (!response.ok) {
-            throw Error(`${response.status} ${response.statusText}`);
-          }
-          return response.json();
-    })
-    .then((products: IProducts[]) => {
-        displayProducts(products);
-        allProducts = products;
-    })
-  }
+  fetch(PRODUCTS_URL)
+  .then((response: Response) => {
+      if (!response.ok) {
+          throw Error(`${response.status} ${response.statusText}`);
+        }
+        return response.json();
+  })
+  .then((products: IProducts[]) => {
+      filteredProducts = products;
+      allProducts = products;
+      isFiltered = false;
+  })
+}
+fetchClothing();
+function formatPrice(price:number):string {
+  return price.toFixed(2);
+}
+
 
   function displayProducts(products: IProducts[]): void  {
     const createEachProduct = products.map((product: IProducts) => {  
@@ -33,7 +58,7 @@ function fetchClothing() {
             <img class="picture" src="${product.image}" alt="">
             <h3>${product.title}</h3>
             <div class="price-and-button-div">
-              <h2>$ ${product.price}</h2>
+              <h2>$ ${formatPrice(product.price)}</h2>
               <button class="add-to-cart">Add to cart</button>
             </div>
         </div>
@@ -42,43 +67,55 @@ function fetchClothing() {
       productDisplay.innerHTML = createEachProduct.join('')
     }
 
-fetchClothing()
+
 
 function displayPressedCategory(products: IProducts[], tag: string) {
-  const displayElectronics = products.map((product: IProducts) => {
-    if(product.category.includes(tag)) {
-      return `
-        <div class="product-container">
-            <img class="picture" src="${product.image}" alt="">
-            <h3>${product.title}</h3>
-            <div class="price-and-button-div">
-              <h2>$ ${product.price}</h2>
-              <button class="add-to-cart">Add to cart</button>
-            </div>
-        </div>
-        `;
-    }
-  });
-  productDisplay.innerHTML = displayElectronics.join('')
+  filteredProducts = products.filter((product: IProducts) => product.category.includes(tag));
+  isFiltered = true; 
+  displayProducts(filteredProducts);
 }
 
+// function displayPressedCategory(products: IProducts[], tag: string) {
+//   const displayElectronics = products.map((product: IProducts) => {
+//     if(product.category.includes(tag)) {
+//       return `
+//         <div class="product-container">
+//             <img class="picture" src="${product.image}" alt="">
+//             <h3>${product.title}</h3>
+//             <div class="price-and-button-div">
+//               <h2>$ ${formatPrice(product.price)}</h2>
+//               <button class="add-to-cart">Add to cart</button>
+//             </div>
+//         </div>
+//         `;
+//     }
+//   });
+//   productDisplay.innerHTML = displayElectronics.join('')
+// }
+
 function searchProducts(products: IProducts[], tag: string) {
-  const displayElectronics = products.map((product: IProducts) => {
-    if(product.title.toLocaleLowerCase().includes(tag.toLocaleLowerCase())) {
-      return `
-        <div class="product-container">
-            <img class="picture" src="${product.image}" alt="">
-            <h3>${product.title}</h3>
-            <div class="price-and-button-div">
-              <h2>$ ${product.price}</h2>
-              <button class="add-to-cart">Add to cart</button>
-            </div>
-        </div>
-        `;
-    }
-  });
-  productDisplay.innerHTML = displayElectronics.join('')
+  filteredProducts = products.filter((product: IProducts) => product.title.toLocaleLowerCase().includes(tag.toLocaleLowerCase()));
+  isFiltered = true; 
+  displayProducts(filteredProducts);
 }
+
+// function searchProducts(products: IProducts[], tag: string) {
+//   const displayElectronics = products.map((product: IProducts) => {
+//     if(product.title.toLocaleLowerCase().includes(tag.toLocaleLowerCase())) {
+//       return `
+//         <div class="product-container">
+//             <img class="picture" src="${product.image}" alt="">
+//             <h3>${product.title}</h3>
+//             <div class="price-and-button-div">
+//               <h2>$ ${formatPrice(product.price)}</h2>
+//               <button class="add-to-cart">Add to cart</button>
+//             </div>
+//         </div>
+//         `;
+//     }
+//   });
+//   productDisplay.innerHTML = displayElectronics.join('')
+// }
 
 electronics.addEventListener('click',(event: Event) => {
   event.preventDefault()
@@ -108,10 +145,52 @@ searchField.addEventListener('input',(event: Event) => {
 sortBy.addEventListener('change', (event: Event) => {
   event.preventDefault();
   let sortedProducts: IProducts[] = [];
+  
   if (sortBy.value === 'lowest') {
-    sortedProducts = [...allProducts].sort((a, b) => a.price - b.price);
+    sortedProducts = isFiltered ? [...filteredProducts].sort((a, b) => a.price - b.price) : [...allProducts].sort((a, b) => a.price - b.price);
   } else if (sortBy.value === 'highest') {
-    sortedProducts = [...allProducts].sort((a, b) => b.price - a.price);
+    sortedProducts = isFiltered ? [...filteredProducts].sort((a, b) => b.price - a.price) : [...allProducts].sort((a, b) => b.price - a.price);
   }
+  
   displayProducts(sortedProducts);
 });
+
+clearFiltersButton.addEventListener('click', (event: Event) => {
+  event.preventDefault();
+  isFiltered = false; 
+  displayProducts(allProducts); 
+});
+
+storeTitle.addEventListener('click', (event: Event) => {
+  event.preventDefault();
+  isFiltered = false; 
+  sortBy.value = ""; 
+  displayProducts(allProducts); 
+});
+
+// sortBy.addEventListener('change', (event: Event) => {
+//   event.preventDefault();
+//   let sortedProducts: IProducts[] = [];
+//   if (sortBy.value === 'lowest') {
+//     sortedProducts = [...allProducts].sort((a, b) => a.price - b.price);
+//   } else if (sortBy.value === 'highest') {
+//     sortedProducts = [...allProducts].sort((a, b) => b.price - a.price);
+//   }
+//   displayProducts(sortedProducts);
+// });
+
+
+//Erster VErsuch
+
+// sortBy.addEventListener('change', (event: Event) => {
+//   event.preventDefault();
+//   let electronicProducts = allProducts.filter ((wert:IProducts) => {
+//     return wert.category.includes ("electronics")
+//     })
+//   if (sortBy.value === 'lowest') {
+//     electronicProducts = [...allProducts].sort((a, b) => a.price - b.price);
+//   } else if (sortBy.value === 'highest') {
+//     electronicProducts = [...allProducts].sort((a, b) => b.price - a.price);
+//   }
+//   displayProducts(electronicProducts);
+// });
